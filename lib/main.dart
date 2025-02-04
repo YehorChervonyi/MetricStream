a3616c67426ff4278a0f5ef49c1599c808fec859
Yehor Chervony <chervony.egor@gmail.com>
2025-02-04 22:31:13 +0200

    Device orientation was set to portrait

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:metricstream_app/pages/home_page.dart';
 
@@ -12,6 +13,7 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
     return  MaterialApp(
       debugShowCheckedModeBanner: false,
       home: HomePage(),
