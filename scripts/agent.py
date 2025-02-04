dd73b6288432ec8f19aca90f0bf9761dbb08f750
Yehor Chervony <chervony.egor@gmail.com>
2025-02-04 23:28:36 +0200

    Updated agent like tray application

--- scripts/agent.py
+++ scripts/agent.py
@@ -1,8 +1,13 @@
-from Info import *
+import pystray
+from pystray import MenuItem as item, Icon
+from PIL import Image
 import asyncio
 import websockets
 import json
 import sys
+from Info import get_ip_address, getInfo
+import threading
+import os
 
 port = 36500
 ip = get_ip_address()
@@ -18,28 +23,53 @@ print("Your IP: " + ip)
 connected_clients = set()
 
 async def handle_client(websocket):
-    """Handles an individual client connection."""
     connected_clients.add(websocket)
     print(f"New client connected: {websocket.remote_address}")
 
     try:
         while True:
-            # Get the data and send it to the client
             data = getInfo()
             json_data = json.dumps(data)
             await websocket.send(json_data)
-            await asyncio.sleep(1)  # Send data every second
+            await asyncio.sleep(1)
     except websockets.exceptions.ConnectionClosed:
         print(f"Client disconnected: {websocket.remote_address}")
     finally:
         connected_clients.remove(websocket)
 
 async def start_server():
-    """Starts the WebSocket server."""
     server = await websockets.serve(handle_client, "0.0.0.0", port)
     print("WebSocket server started on ws://"+ip+":"+str(port))
-    await server.wait_closed()  # Keep the server running
+    await server.wait_closed()
 
+def create_image_from_path(image_path):
+    try:
+        image = Image.open(image_path)
+        image = image.convert("RGBA")
+        return image
+    except Exception as e:
+        print(f"Error loading image: {e}")
+        return None
 
+def quit_application(icon, item):
+    icon.stop()
+    sys.exit()
 
-asyncio.run(start_server())
+menu = (
+        item("MetricStream Agent", lambda icon, item: None, enabled=False), 
+        item(f"IP: {ip}:{port}", lambda icon, item: None, enabled=False),
+        item("Quit", quit_application)
+    )
+
+icon_path = os.path.join(os.path.dirname(__file__),'..','assets', 'Icon.png')
+icon = Icon("MetricStream Agent", create_image_from_path(icon_path), menu=menu)
+
+icon.title = f"MetricStream Agent\nIP: {ip}:{port}"
+
+def run_server_in_thread():
+    asyncio.run(start_server())
+
+server_thread = threading.Thread(target=run_server_in_thread, daemon=True)
+server_thread.start()
+
+icon.run()
